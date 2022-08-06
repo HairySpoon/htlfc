1ebf579cf9b91f94a8b037a90859b09fc19a0461
HairySpoon <107159263+HairySpoon@users.noreply.github.com>
2022-08-06 12:52:46 +0930

    Adopt file VERSION in lieu of hard coded.
    Corresponding changes to pyproject.toml and version.py

--- src/htlfc/version.py
+++ src/htlfc/version.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-"""Single point of reference for version string"""
-__version__ = "0.1.0" # used by setuptools in pyproject.toml
+"""The file VERSION in this directory, is kept current by GitHub workflows.
+It is read directly by setuptools in pyproject.toml
+get_version() is called from main()
+"""
+import os
 
 def get_version():
-    return __version__
-
+    root = os.path.split(__file__)[0]
+    vers_file = os.path.join(root, "VERSION")
+    with open(vers_file,'r') as fp:
+        version = fp.readline()
+    return version
